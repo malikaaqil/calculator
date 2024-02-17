a90321e28a9cdede694a8a23f3ff74502cf6a642
Malik Aaqil <malikaaqil2000@gmail.com>
2024-02-17 09:39:48 +0500

    added JS

---
+++ app.js
@@ -0,0 +1,19 @@
+let display = document.querySelector(".display");
+
+
+function appendToDisplay(input){
+
+    display.value += input;
+
+}
+
+function clearDisplay(){
+    
+}
+
+function deleteVal(){
+
+}
+function calculate(){
+
+}
